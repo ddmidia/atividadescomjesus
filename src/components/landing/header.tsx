df1cb3c717717576adc2106df03c6557dcd0377b
Lucas Domingos <lucasdomingos07@gmail.com>
2025-10-26 02:30:39 +0000

    Use o icone do raio no estilo da ampulheta para ficar armonico, use um d

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -1,10 +1,11 @@
 import CountdownTimer from "./countdown-timer";
-import { Hourglass } from "lucide-react";
+import { Hourglass, Zap } from "lucide-react";
 
 export default function Header() {
   return (
-    <header className="py-2 px-4 md:px-6 bg-footer text-white sticky top-0 z-40">
+    <header className="py-2 px-4 md:px-6 bg-gradient-to-b from-footer/80 to-footer text-white sticky top-0 z-40">
       <div className="container mx-auto flex items-center justify-center text-center">
+        <Zap className="w-4 h-4 mr-2" />
         <span className="font-semibold text-sm sm:text-base">DESCONTO ATIVADO ⚡ ENCERRA EM&nbsp;</span>
         <CountdownTimer initialMinutes={19} initialSeconds={59} />
         <Hourglass className="w-4 h-4 ml-2" />
