3e89656bcae8f3d791150be461495fb13a25fd19
Lucas Domingos <lucasdomingos07@gmail.com>
2025-10-26 02:33:42 +0000

    Agora use o efeito do degrade estilo dos botoes mantendo a cor azul

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -2,7 +2,7 @@ import CountdownTimer from "./countdown-timer";
 
 export default function Header() {
   return (
-    <header className="py-2 px-4 md:px-6 bg-gradient-to-b from-footer to-footer text-white sticky top-0 z-40">
+    <header className="py-2 px-4 md:px-6 bg-gradient-to-b from-[#222b4e] to-footer text-white sticky top-0 z-40">
       <div className="container mx-auto flex items-center justify-center text-center gap-2">
         <span className="font-semibold text-sm sm:text-base">
           DESCONTO ATIVADO ⚡ ENCERRA EM
