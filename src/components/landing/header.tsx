868a088163c1064cd756cd9c5c51f20d5bdd3c86
Lucas Domingos <lucasdomingos07@gmail.com>
2025-10-26 02:32:26 +0000

    Eu qeuro assim "DESCONTO ATIVADO "EMOJO RAIO" ENCARRA EM 19:59 "EMOJI EM

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -1,14 +1,14 @@
 import CountdownTimer from "./countdown-timer";
-import { Hourglass, Zap } from "lucide-react";
 
 export default function Header() {
   return (
     <header className="py-2 px-4 md:px-6 bg-gradient-to-b from-footer/80 to-footer text-white sticky top-0 z-40">
-      <div className="container mx-auto flex items-center justify-center text-center">
-        <Zap className="w-4 h-4 mr-2" />
-        <span className="font-semibold text-sm sm:text-base">DESCONTO ATIVADO - ENCERRA EM&nbsp;</span>
+      <div className="container mx-auto flex items-center justify-center text-center gap-2">
+        <span className="font-semibold text-sm sm:text-base">
+          DESCONTO ATIVADO ⚡ ENCERRA EM
+        </span>
         <CountdownTimer initialMinutes={19} initialSeconds={59} />
-        <Hourglass className="w-4 h-4 ml-2" />
+        <span className="text-sm sm:text-base">⏳</span>
       </div>
     </header>
   );
