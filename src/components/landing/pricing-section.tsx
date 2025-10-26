8c815f6c35c90f49ed3d646722f7afdab0d66453
Lucas Domingos <lucasdomingos07@gmail.com>
2025-10-26 19:50:07 +0000

    Ficou errado, corriga, precisa ser na horizontal

--- src/components/landing/pricing-section.tsx
+++ src/components/landing/pricing-section.tsx
@@ -66,7 +66,7 @@ export default function PricingSection() {
             )}>
               {plan.isRecommended && (
                 <>
-                  <div className="absolute top-0 right-0 h-24 w-24 overflow-hidden z-10">
+                   <div className="absolute top-0 right-0 h-24 w-24 overflow-hidden z-10">
                     <div className="absolute top-6 -right-5 w-full bg-accent text-accent-foreground text-xs font-bold text-center transform rotate-45 py-1 shine-effect">
                         MAIS VENDIDO
                     </div>
