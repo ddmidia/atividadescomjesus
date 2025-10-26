46837bac88a8d2256fadae657a583960b63d3588
Lucas Domingos <lucasdomingos07@gmail.com>
2025-10-26 02:22:56 +0000

    O TEMPO FICOU GRUDADO COM A LETRA M, separe. E coloque "SUA" ANTES DE OF

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -1,10 +1,12 @@
 import CountdownTimer from "./countdown-timer";
+import { Hourglass } from "lucide-react";
 
 export default function Header() {
   return (
     <header className="py-2 px-4 md:px-6 bg-footer text-white sticky top-0 z-40">
       <div className="container mx-auto flex items-center justify-center text-center">
-        <span className="font-semibold text-sm sm:text-base">OFERTA ENCERRA EM </span>
+        <Hourglass className="w-4 h-4 mr-2" />
+        <span className="font-semibold text-sm sm:text-base">SUA OFERTA ENCERRA EM&nbsp;</span>
         <CountdownTimer initialMinutes={14} initialSeconds={35} />
       </div>
     </header>
