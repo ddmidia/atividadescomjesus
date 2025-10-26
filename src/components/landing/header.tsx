d2995ec7f89cf43b7e52852fde55ca7e80604b56
Lucas Domingos <lucasdomingos07@gmail.com>
2025-10-26 02:43:43 +0000

    Deixe um rosa mais claro

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -3,10 +3,10 @@ import CountdownTimer from "./countdown-timer";
 
 export default function Header() {
   return (
-    <header className="py-2 px-4 md:px-6 bg-gradient-to-b from-[#222b4e] to-footer text-white sticky top-0 z-40">
+    <header className="py-2 px-4 md:px-6 bg-gradient-to-b from-[#2a345c] to-footer text-white sticky top-0 z-40">
       <div className="container mx-auto flex items-center justify-center text-center gap-2">
         <span className="font-semibold text-sm sm:text-base flex items-center gap-2">
-          DESCONTO ATIVADO <Zap size={16} className="inline-block text-accent" /> ENCERRA EM
+          DESCONTO ATIVADO <Zap size={16} className="inline-block text-pink-300" /> ENCERRA EM
         </span>
         <CountdownTimer initialMinutes={19} initialSeconds={59} />
       </div>
