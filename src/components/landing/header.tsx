5c6fd370cf47dceda927835eb595452102d6cc76
Lucas Domingos <lucasdomingos07@gmail.com>
2025-10-26 02:29:03 +0000

    TROQUE POR "DESCONTO ATIVADO ⚡ENCERRA EM 19:59" e o icone da ampulheta n

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -5,9 +5,9 @@ export default function Header() {
   return (
     <header className="py-2 px-4 md:px-6 bg-footer text-white sticky top-0 z-40">
       <div className="container mx-auto flex items-center justify-center text-center">
-        <Hourglass className="w-4 h-4 mr-2" />
-        <span className="font-semibold text-sm sm:text-base">SUA OFERTA ENCERRA EM&nbsp;</span>
+        <span className="font-semibold text-sm sm:text-base">DESCONTO ATIVADO ⚡ ENCERRA EM&nbsp;</span>
         <CountdownTimer initialMinutes={19} initialSeconds={59} />
+        <Hourglass className="w-4 h-4 ml-2" />
       </div>
     </header>
   );
