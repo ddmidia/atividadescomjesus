92760a99bed0a96b6dad9f5a16e155ac204537b0
Lucas Domingos <lucasdomingos07@gmail.com>
2025-10-29 18:19:03 +0000

    Subistitua a frase da subheadline "São mais de 100 Atividades Bíblicas p

--- src/components/landing/hero-section.tsx
+++ src/components/landing/hero-section.tsx
@@ -16,10 +16,10 @@ export default function HeroSection() {
         <div className="flex flex-col space-y-6 text-center md:items-center">
           <div className="space-y-4 text-center">
             <h1 className="text-3xl md:text-4xl font-headline font-bold tracking-tight text-headline">
-              Seus filhos passam tempo<br className="md:hidden" /> demais nas telas?
+              Seus filhos passam tempo<br className="sm:hidden" /> demais nas telas?
             </h1>
             <h2 className="text-lg md:text-xl text-muted-foreground max-w-3xl mx-auto">
-              <strong className="text-foreground">Descubra como transformar essa distração<br className="md:hidden" /> em devoção com o Kit Colorindo com Fé e<br className="md:hidden" /> Alegria —</strong> São mais de 100 Atividades Bíblicas<br className="md:hidden" /> para Colorir e Ensinar sobre Jesus.
+              <strong className="text-foreground">Descubra como transformar essa distração<br className="md:hidden" /> em devoção com o Kit Colorindo com Fé e<br className="md:hidden" /> Alegria —</strong> São + de 100 Atividades Bíblicas que ensinam sobre a fé de Jesus.
               <br />
               <br className="hidden md:block" />
               📝 Basta imprimir e se divertir!
