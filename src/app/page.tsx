a075c8ea9c07aa4cfd22232aacfb9a341143d445
Lucas Domingos <lucasdomingos07@gmail.com>
2025-10-29 12:56:45 +0000

    Mova a sessão dos depoimentos para abaixo do carrossel de desenhos.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,9 +20,9 @@ export default function Home() {
         <HeroSection />
         <CreatorSection />
         <ColoringPagesSection />
+        <TestimonialsSection />
         <BenefitsSection />
         <WhatYouGetSection />
-        <TestimonialsSection />
         <PricingSection />
         <GuaranteeSection />
         <FaqSection />
